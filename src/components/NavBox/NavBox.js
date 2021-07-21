17a815256a0abf3e88b3a6448f8981282b0f241c
shubham6feb123 <maarahuls555@gmail.com>
2021-07-21 08:14:42 +0000

    personal portfolio

--- src/components/NavBox/NavBox.js
+++ src/components/NavBox/NavBox.js
@@ -5,7 +5,10 @@ function NavBox({show}) {
      return (
          <>
         <div className={show?'navBox showNavBox':'navBox'}>
-            
+            <div className="icon__1">ye mera</div>
+            <div className="icon__2">portfolio</div>
+            <div className="icon__3">hai</div>
+            <div className="icon__4">bahi ji</div>
         </div>
         </>
     )
