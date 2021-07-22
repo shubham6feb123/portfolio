a126622e588b602f84999fbd532fec94a491cdba
shubham6feb123 <maarahuls555@gmail.com>
2021-07-22 16:16:59 +0000

    personal portfolio

--- src/components/user/User.js
+++ src/components/user/User.js
@@ -1,15 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './User.css';
 
 function User() {
+    const [authState,setAuthState] = useState('Sign In');
+    const userAuthAction = (e)=>{
+    //   console.log('value',e.target.innerText)
+      setAuthState(e.target.innerText);
+    }
     return (
         <div className="user__wrapper">
             <div className="user">
                 <div className="user__tabs">
-                    <div className="user__tabs__sign__in">Sign In</div>
-                    <div className="user__tabs__sign__up">Sign Up</div>
+                    <div className="user__tabs__sign__in" onClick={userAuthAction}>Sign In</div>
+                    <div className="user__tabs__sign__up" onClick={userAuthAction}>Sign Up</div>
                 </div>
-              user hu main
+              {authState==="Sign In"?"sign In":"sign up"}
             </div>
         </div>
     )
