ec62da94bea166c36ea02ae2ba8b2063da18af66
shubham6feb123 <maarahuls555@gmail.com>
2021-07-31 06:21:44 +0000

    personal portfolio backend

---
+++ backend/server.js
@@ -0,0 +1,11 @@
+const app  = require('express')();
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+app.get('/',(req,res)=>{
+    res.send('nmaste duniya');
+})
+
+app.listen(5000,()=>{
+    console.log("server is listening on port 5000")
+});
